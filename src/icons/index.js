1200c0d8bca41ab0ee5fde668fa432e7f5f4ddd8
Sumer Cetin <sumercetin3@gmail.com>
2023-07-16 16:49:51 +0300

    TodoApp :: update icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -2,3 +2,4 @@ export { ReactComponent as CheckIcon } from './Check.svg';
 export { ReactComponent as PlusIcon } from './Plus.svg';
 export { ReactComponent as DragIcon } from './Drag.svg';
 export { ReactComponent as MoreIcon } from './More.svg';
+export { ReactComponent as ChevronDownIcon } from './ChevronDown.svg';
